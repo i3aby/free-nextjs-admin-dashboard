8ed8357047b78d980f6734ec559be77040ce560e
i3aby <blackmailhersister@gmail.com>
2026-05-20 23:44:35 +0500

    Update UserDropdown.tsx

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -60,7 +60,7 @@ function toggleDropdown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
       >
         <div>
           <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-            Musharof Chowdhury
+            Tayyab Khan
           </span>
           <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400">
             randomuser@pimjo.com
